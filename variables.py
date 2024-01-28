50e3c38233d5a6c4218f07c42f1e6bfb158b2374
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-01-28 23:39:39 +0300

    Update variables.py

--- variables.py
+++ variables.py
@@ -4,6 +4,16 @@ a = 20 # integer
 b = 50 # integer
 name = "caleb"  # string
 y = True  # boolean
+w = 1.550 # this is a float
 
 # there are multiple data types in python
 # strings are enclosed in double or single quotation marks
+
+"""multiple assignemnts"""
+
+q,w,e = 1,2,3
+
+"""variable reassignemt"""
+
+t = 100
+t = t + 1
