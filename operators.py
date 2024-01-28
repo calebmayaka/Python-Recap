7e7150b1c9256b46592c74c93195936f43c456ea
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-01-28 23:48:18 +0300

    Create operators.py

---
+++ operators.py
@@ -0,0 +1,17 @@
+# there are multiple types of operators in python
+
+"""assignent operators"""
+
+x = 5
+y = 20
+y = y + 1
+y -= 10
+print(y)
+
+"""logical operators"""
+
+a = 10
+b = 20
+
+if a>9 and b==20:
+    print("condition is true")
