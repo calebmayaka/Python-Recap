d25c6fcfd4bfb25647c18b9d39ddca99af9fc650
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-01-28 23:37:26 +0300

    comments and variables

---
+++ comments.py
@@ -0,0 +1,3 @@
+#h thisis a single line comment in python
+"""this is a multi line comment in python or a 
+docstring"""
---
+++ variables.py
@@ -0,0 +1,9 @@
+# a variable is assigned when it is created
+
+a = 20 # integer
+b = 50 # integer
+name = "caleb"  # string
+y = True  # boolean
+
+# there are multiple data types in python
+# strings are enclosed in double or single quotation marks
