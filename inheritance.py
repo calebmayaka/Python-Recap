4dde47dec94b52b3ab00ffa10d13ffe9848093e6
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-01-29 13:15:20 +0300

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,17 @@
+# inheritance is an Object oriented concept in which there is a main class and child classes
+# child classes inheriat properties from the main class
+
+class employee:
+    def __init__(self,firstname,lastname,dob):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.dob = dob
+    def printlastname(self):
+        full = self.firstname, self.lastname
+        return full
+fname = str(input("Enter your firstname: "))
+lname = str(input("enter your last name: ")) 
+  
+
+
+employee1 = employee(fname,lname,dob)
