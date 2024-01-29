a91284a062969fa7c0ebfb52db2decfcfe632934
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-01-29 20:51:59 +0300

    Update class.py

--- class.py
+++ class.py
@@ -15,6 +15,12 @@ class developer(employee):
         self.language = language
     
 name_input = str(input("Enter your full name: "))
+dob_input = str(input("Enter your full name: "))
+town_input = str(input("Enter your full name: "))
+language_input = str(input("Enter your full name: "))
+
 
 developer1 = developer(name_input,dob_input,town_input,language_input)
+
+print(developer1.printemployeedetails())
     
