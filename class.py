eed98e55c72d43bed51c2f0dd9d2327717572f27
Caleb Mayaka <mayakaombogo254@gmail.com>
2024-01-29 20:40:42 +0300

    Create class.py

---
+++ class.py
@@ -0,0 +1,8 @@
+class employee:
+    def __init__(self,name,dob,town):
+        self.name = name
+        self.dob = dob
+        self.town = town
+        
+    def printemployeedetails(self):
+        details_print = 
